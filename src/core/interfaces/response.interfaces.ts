2f136dbe0eb9351378a2b56a26cb858a4f8ee2e6
Atik Shahriar Ratul <ratul@Atiks-MacBook-Pro.local>
2024-09-12 19:54:55 +0600

    feat(add-files): add files
    files

---
+++ src/core/interfaces/response.interfaces.ts
@@ -0,0 +1,23 @@
+// biome-ignore lint/style/useImportType: <explanation>
+import { HttpStatus } from '@nestjs/common';
+
+export interface IResponseMetadata {
+    statusCode?: HttpStatus;
+    message?: string;
+}
+
+export interface IResponse {
+    _metadata?: IResponseMetadata;
+    _data?: Record<string, any>;
+}
+
+export interface IResponsePaging {
+    _metadata?: IResponseMetadata;
+    _pagination: IResponsePagingPagination;
+    _data: Record<string, any>[];
+}
+
+export interface IResponsePagingPagination {
+    totalPage: number;
+    total: number;
+}
